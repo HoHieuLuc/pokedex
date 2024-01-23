42bb6dc4e7467cff4ad714fcd406602ead367cd0
Luc <luchohieu@gmail.com>
2024-01-23 20:57:25 +0700

    add use-navigate hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useItemPicker } from './use-item-picker/use-item-picker';
 export { default as useStateRef } from './use-state-ref/use-state-ref';
 export { default as useSelectedIndex } from './use-selected-index/use-selected-index';
+export { default as useNavigate } from './use-navigate/use-navigate';
---
+++ src/hooks/use-navigate/use-navigate.ts
@@ -0,0 +1,39 @@
+import { useSessionStorage } from '@mantine/hooks';
+import { NavigateOptions } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { usePathname, useRouter } from 'next/navigation';
+
+interface Props {
+  defaultValue: string;
+}
+
+const useNavigate = ({ defaultValue }: Props) => {
+  const router = useRouter();
+  const path = usePathname();
+  const [previousUrl, setPreviousUrl] = useSessionStorage({
+    key: 'previousUrl',
+    defaultValue,
+  });
+
+  /**
+   * Navigate and store the previous url in session storage.
+   */
+  const navigate = (href: string, options?: NavigateOptions) => {
+    setPreviousUrl(path);
+    router.push(href, options);
+  };
+
+  /**
+   * Go back to the previous url in session storage.
+   */
+  const back = () => {
+    router.push(previousUrl);
+  };
+
+  const push = (href: string, options?: NavigateOptions) => {
+    router.push(href, options);
+  };
+
+  return { previousUrl, navigate, back, push };
+};
+
+export default useNavigate;
