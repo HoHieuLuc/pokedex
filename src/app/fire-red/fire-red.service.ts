35b37b681d4d614cc9a002afe666dc56a6f14474
Luc <luchohieu@gmail.com>
2024-01-25 21:16:31 +0700

    [fire-red] replace fairy type with normal type

--- src/app/fire-red/fire-red.service.ts
+++ src/app/fire-red/fire-red.service.ts
@@ -1,9 +1,13 @@
-import { pokemonService } from '@/pokemon';
+import { PokemonTypeData, pokemonService } from '@/pokemon';
 import { FireRedDex } from './fire-red.type';
 import { POKEDEX_RANGES } from '@/config';
 
 const getDex = async (dex: FireRedDex) => {
   const pokemons = await pokemonService.getAll();
+  const normalType: PokemonTypeData = {
+    slot: 1,
+    type: { name: 'normal' },
+  };
 
   return pokemons
     .filter((pokemon) => {
@@ -15,6 +19,15 @@ const getDex = async (dex: FireRedDex) => {
     .slice(0, dex === 'kanto' ? POKEDEX_RANGES.kanto.max : POKEDEX_RANGES.fireRedNational.max)
     .sort((a, b) => {
       return a.species.pokedexNumbers[dex] - b.species.pokedexNumbers[dex];
+    })
+    .map((pokemon) => {
+      // remove fairy type
+      const types = pokemon.types.filter((type) => type.type.name !== 'fairy');
+
+      return {
+        ...pokemon,
+        types: types.length === 0 ? [normalType] : types,
+      };
     });
 };
 
