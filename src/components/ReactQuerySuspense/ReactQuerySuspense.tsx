6a56313d82e732e4c75d5cb24b4e08e2d46c43ca
Luc <luchohieu@gmail.com>
2024-01-29 18:13:57 +0700

    [components] ReactQuerySuspense: fix empty data

--- src/components/ReactQuerySuspense/ReactQuerySuspense.tsx
+++ src/components/ReactQuerySuspense/ReactQuerySuspense.tsx
@@ -1,17 +1,36 @@
 'use client';
 
-import { QueryKey, useQuery } from '@tanstack/react-query';
+import {
+  QueryClient,
+  QueryFunction,
+  QueryKey,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
 
 interface Props {
   queryKey: QueryKey;
+  queryFn?: QueryFunction;
   fallback?: React.ReactNode;
   error?: React.ReactNode;
   children?: React.ReactNode;
 }
 
-const ReactQuerySuspense = ({ queryKey, fallback, error, children }: Props) => {
+const defaultQueryFn = (queryKey: QueryKey, queryClient: QueryClient) => {
+  return () => {
+    const data = queryClient.getQueryData(queryKey);
+    if (data === undefined) {
+      return {};
+    }
+    return data;
+  };
+};
+
+const ReactQuerySuspense = ({ queryKey, queryFn, fallback, error, children }: Props) => {
+  const queryClient = useQueryClient();
   const { data, isLoading, isError } = useQuery({
     queryKey,
+    queryFn: queryFn || defaultQueryFn(queryKey, queryClient),
   });
 
   if (isError) {
